aac589576b14f6b28b31dd1e5823cfe7fe9dbed7
Gabin Rousseau <s2013679@bioinfmsc8.bio.ed.ac.uk>
2025-04-07 23:50:54 +0100

    Implemented basic conservation analysis

---
+++ help.php
@@ -0,0 +1,20 @@
+<html>
+<title>PSAW Help</title>
+
+<header>
+<link rel="icon" href="psaw2.png">
+</header>
+<body>
+
+<!-- Display Menu -->
+<div class="menu">
+<?php include 'menu.php';?>
+</div>
+
+
+<?php
+phpinfo();
+?>
+
+</body>
+</html>
--- menu.php
+++ menu.php
@@ -8,18 +8,22 @@ echo <<<_MENU
     <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/Website/fetch.php"> Fetch Sequences </a>
     </div></td>
    <td bgcolor="#DCEFFE"><div align="center">
-    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/DL/p2.php"> TBD </a>
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/Website/conservation.php"> Conservation </a>
     </div></td>
    <td bgcolor="#DCEFFE"><div align="center">
-   <td bgcolor="#DCEFFE"><div align="center">
-    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/DL/p3.php"> TBD </a>
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/Website/motifs.php"> Motifs </a>
     </div></td>
    <td bgcolor="#DCEFFE"><div align="center">
-   <td bgcolor="#DCEFFE"><div align="center">
-    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/DL/p4.php"> TBD </a>
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/Website/tmap.php"> Transmembrane domains </a>
     </div></td>
    <td bgcolor="#DCEFFE"><div align="center">
-    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/DL/p5.php"> Exit </a>
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/Website/help.php"> Help </a>
+    </div></td>
+   <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/Website/about.php"> About </a>
+    </div></td>
+   <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2013679/Website/credits.php"> Statement of Credits </a>
     </div></td>
 </tr></table>
 _MENU;
